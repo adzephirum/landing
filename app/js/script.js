ac53f1c7adc87c9d11667cd24aa16fdeac0279fb
dmdgarcia <davidmd.garcia@gmail.com>
2021-05-09 14:15:17 +0900

    add script for humburger

--- app/js/script.js
+++ app/js/script.js
@@ -2,5 +2,11 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 
 btnHamburger.addEventListener("click", function () {
-  console.log("open");
+  console.log("click humburger");
+
+  if (btnHamburger.classList.contains("open")) {
+    btnHamburger.classList.remove("open");
+  } else {
+    btnHamburger.classList.add("open");
+  }
 });
