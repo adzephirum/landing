9b534d3751ab94ffed10ee898f12a14c8f3f98e4
dmdgarcia <davidmd.garcia@gmail.com>
2021-05-15 16:54:25 +0900

    update animations and update script

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,20 @@
 // console.log("hello world");
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
 
 btnHamburger.addEventListener("click", function () {
   console.log("click humburger");
 
   if (header.classList.contains("open")) {
+    // close hemburger menu
     header.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
   } else {
+    // open hamburger menus
     header.classList.add("open");
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
   }
 });
