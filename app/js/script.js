210f6d4b2e060d6b1798997782b0a47a85fbfe2c
dmdgarcia <davidmd.garcia@gmail.com>
2021-06-04 18:51:49 +0900

    add noscroll class and script

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,6 @@
 // console.log("hello world");
 const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".has-fade");
@@ -9,6 +10,7 @@ btnHamburger.addEventListener("click", function () {
 
   if (header.classList.contains("open")) {
     // close hemburger menu
+    body.classList.remove("noscroll");
     header.classList.remove("open");
     fadeElems.forEach(function (element) {
       element.classList.remove("fade-in");
@@ -16,6 +18,7 @@ btnHamburger.addEventListener("click", function () {
     });
   } else {
     // open hamburger menus
+    body.classList.add("noscroll");
     header.classList.add("open");
     fadeElems.forEach(function (element) {
       element.classList.remove("fade-out");
